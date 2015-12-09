1d118a2eaecd9d8a3be6921bdb89b580396970e0
Leandro.Battisti <leandro.battisti@senior.com.br>
2015-12-09 10:29:11 -0200

    Adicionado video para o botão arreguei

--- acertei.php
+++ acertei.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <head>
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -11,24 +11,15 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
 <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body class="body-fundo1">
-<div class="container centered limit-text" align="center">
-  <br>
-  <br>
-  <h1>Este é um desafio.</h1>
-
-  <h2>Gielez, desenvolvi esta página para você receber teu presente de NATAL</h2>
-  <h2>Para descobrir o presente precisas decifrar o código para saber o link da próxima página.</h2>
-  <form action="comecar.php">
-    <button type="submit" class="btn btn-lg btn-success">Começar</button>
-  </form>
-  <form action="acertei.php">
-    <button type="submit" class="btn btn-lg btn-danger">Arreguei</button>
-  </form>
-
-</div>
-
+    <div class="container centered limit-text" align="center">
+      <video width="480" height="320" controls autoplay>
+        <source src="images/acertei.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
 </body>
 </html>
